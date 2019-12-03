9629c842e908940c1194c95c69984ce82653546b
Edward Stevens <edwardstevens@Edwards-MacBook-Pro.local>
2019-12-03 15:11:11 +0700

    project modal and models added

--- src/App.js
+++ src/App.js
@@ -9,42 +9,14 @@ import { Row, Col, Button } from 'react-bootstrap';
 import './App.css';
 
 
-
-
 function App() {
   const [user, setUser] = useState(null)
-    // token: existingToken || accessToken,
   ;
 
   useEffect(() => {
     getUserInfo();
     window.history.replaceState({}, document.title, '/');
-    // if (!user.email) {
-    //   const existingToken = localStorage.getItem("token");
-    //   // const accessToken =
-    //   //   window.location.search.split("=")[0] === "?api_key"
-    //   //     ? window.location.search.split("=")[1]
-    //   //     : null;
-
-    //   if (accessToken) {
-    //     localStorage.setItem("token", accessToken);
-    //     console.log("accesstoken", accessToken)
-    //   }
-
-    //   if (existingToken) {
-    //     console.log("existingtoken", existingToken)
-    //     setUser({ token: existingToken });
-    //     getUserInfo(existingToken)
-    //   }
-    // }
   }, [])
-  console.log("user", user)
-
-  // useEffect(() => {
-  //   getUserInfo()
-  //   console.log("user", user)
-
-  // }, [])
 
   const doLogOut = async ( ) => {
     const resp = await fetch("https://127.0.0.1:5000/logout", {
@@ -59,38 +31,8 @@ function App() {
           localStorage.clear('token')
             setUser(null)
         }
-
     }
   }
-
-  console.log('user in function', user)
-
-  // postScore = async (wpm, elapsed) => { Using as example of post request 
-  //   const resp = await fetch("https://127.0.0.1:5000/scores", {
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Token ${this.state.token}`
-  //     },
-  //     body: JSON.stringify({
-  //       wpm,
-  //       time: elapsed,
-  //       errorCount: this.state.errorCount,
-  //       excerpt_id: this.state.excerpt.id
-  //     })
-  //   });
-  //   const data = await resp.json();
-  //   console.log('score response', data)
-  //   if (resp.status === 200) {
-      
-  //     this._restartGame();
-  //     // this.setState({
-  //     //     statwpm: data.
-  //     // });
-  //   } else {
-  //     this.setState({ error: "Could not post score" });
-  //   }
-  // };
 
   const getUserInfo = async () => {
     const existingToken = localStorage.getItem("token");
@@ -98,14 +40,13 @@ function App() {
         window.location.search.split("=")[0] === "?api_key"
           ? window.location.search.split("=")[1]
           : null;
-    console.log(existingToken, accessToken)
     const resp = await fetch("https://127.0.0.1:5000/getuserinfo", {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Token ${existingToken || accessToken}`
       }
     })
-    console.log('edoieieoiei', resp)
+
     if (resp.ok) {
       const data = await resp.json()
       localStorage.setItem("token", data.token)
@@ -114,7 +55,7 @@ function App() {
       localStorage.clear("token")
     }
   }
-  console.log('user in function', user)
+  
 
 
   if (!user) {
@@ -126,24 +67,22 @@ function App() {
         user={user}
         setUser={setUser}
         />
-        {/* <Button className="fbloginbt"variant="primary" size="lg"  onClick={() => window.location.replace('https://127.0.0.1:5000/login/facebook')}> Login with Facebook</Button> */}
       </Col>
       </Row>
       </>
     )
   }
   return (
-
     <>
-
       <Navi
         user={user}
         doLogOut={doLogOut}
       />
-      {/* <Button onClick={() => window.location.replace('https://127.0.0.1:5000/logout')}> Logout</Button>  Test completed and it works can remove later */}
       <Row style={{ height: '90vh' }}>
         <Col className='projects' md={2}>
-          <Projects />
+          <Projects 
+          user={user}
+          />
         </Col>
         <Col md='10'>
           <Row style={{ height: '100vh' }}>
--- src/components/HomeProjects.js
+++ src/components/HomeProjects.js
@@ -1,15 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaPlusCircle, FaFilter } from 'react-icons/fa';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap'
 
-const Projects = () => {
+const Projects = (props) => {
+    const [show, setShow] = useState(false);
+    const [input, setInput] = useState({})
+    const [validated, setValidated] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleOnChange = (e) => {
+        
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        const form = e.currentTarget;
+        console.log('form.checkValidity()', form.checkValidity())
+        console.log('input',input)
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation();
+            setValidated(true);
+            console.log('e-target',e.target.title.value )
+            setInput({
+                title: e.target.title.value ,
+                description: e.target.description.value ,
+                startdate: e.target.startdate.value ,
+                enddate: e.target.enddate.value ,
+            })
+            return
+        }
+        e.preventDefault()
+        return (createProject(e), handleClose())
+    }
+    const createProject = async (e) => {
+        console.log('input in create project',input)
+        const form = e.currentTarget;
+        console.log('form.checkValidity()', form.checkValidity())
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation();
+        }
+        e.preventDefault()
+        setValidated(true);
+        const resp = await fetch("https://127.0.0.1:5000/newproject", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(input)
+        })
+        const data = await resp.json()
+        if (data.success) {
+            setInput({
+                title: '',
+                description: '',
+                startdate: null,
+                enddate: null,
+            })
+            setValidated(false)  //push to project view wit            h its done, todo, doing etc 
+        }
+    }
+
     return (
         <>
             <h1>Projects</h1>
-            <h6> <FaPlusCircle />  <FaFilter /></h6>
+            <h6>  <FaFilter /></h6>
+            <Button variant="primary" onClick={handleShow}>
+                <FaPlusCircle /> Create a Project
+            </Button>
+            <Modal
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered show={show} onHide={handleClose}>
+                <Form noValidate validated={validated} className="projectform" onChange={(e) => handleOnChange(e)} onSubmit={(e) => handleSubmit(e)}>
+                    <h4>Create a new project </h4>
+                    <img className="mb-4" src="/client/src/images/Logo.png'" alt="" width="72" height="72" />
+
+                    <Form.Group controlId="formProjectTitle">
+                        <Form.Control required name="title" type="text" placeholder="Project Title (required)" />
+                        <Form.Control.Feedback type="invalid">Every project needs a title!</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="formProjectDescription">
+                        <Form.Control name="description" type="text" placeholder="Describe The Project" />
+                    </Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="formProjectStart">
+                                <Form.Label>Project Start Date</Form.Label>
+                                <Form.Control required name="startdate" type="date" placeholder="Start Date" />
+                                <Form.Control.Feedback type="invalid">Every project needs a startdate</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="formProjectEnd">
+                                <Form.Label>Project Deadline</Form.Label>
+                                <Form.Control required name="enddate" type="date" placeholder="End Date" />
+                                <Form.Control.Feedback type="invalid">Every project needs a deadline</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Button block size="lg" variant="success" type="submit"  >
+                        Create
+                    </Button>
+                </Form>
+            </Modal>
             <hr />
+            
             <p>Display projects here in a list - default open projects</p>
         </>
     );
 }
 
 export default Projects
+
